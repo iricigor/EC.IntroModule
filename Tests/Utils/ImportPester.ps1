a7e6578498a9dd680c39a5909463c04bca27abac
Igor Irić <iricigor@gmail.com>
2022-11-27 00:23:44 +0100

    more verbose installation

--- Tests/Utils/ImportPester.ps1
+++ Tests/Utils/ImportPester.ps1
@@ -1,8 +1,11 @@
 $module = Get-Module -Name Pester -ListAvailable | Where-Object Version -ge 5.0.0
 
-if (!$module) {
-    Write-Warning "Pester module not found. Installing Pester module..."
+if ($module) {
+    Write-Host "Pester module found. Importing Pester module..."
+    Import-Module -Name $module.Name -RequiredVersion $module.Version -Force
+} else {
+    Write-Host "Pester module not found. Installing Pester module..."
     Install-Module -Name Pester -Force -Scope CurrentUser -RequiredVersion 5.0.0
+    Import-Module -Name Pester -Force -Scope Global -RequiredVersion 5.0.0
 }
 
-Import-Module -Name Pester -Force -Scope Global -RequiredVersion 5.0.0
